c94d0c8e76eea37b927e1340eae1f1c666d847bc
wurunDUK <wurunduk@gmail.com>
2018-07-27 00:11:49 +0300

    map size tool gui

--- Assets/Scripts/Tools/Tool_ChangeMapSize.cs
+++ Assets/Scripts/Tools/Tool_ChangeMapSize.cs
@@ -6,6 +6,8 @@ public class Tool_ChangeMapSize : ToolGeneral
 {
 	public int SelectedTileId;
 
+	private int _addRight, _addLeft, _addDown, _addUp;
+
 	public override void Initialize()
 	{
 		ToolName = "Map size";
@@ -13,7 +15,10 @@ public class Tool_ChangeMapSize : ToolGeneral
 
 	public override void OnSelected()
 	{
-		
+		_addRight = 0;
+		_addLeft = 0;
+		_addDown = 0;
+		_addUp = 0;
 	}
 
 	public override void OnDeselected()
@@ -43,6 +48,44 @@ public class Tool_ChangeMapSize : ToolGeneral
 
 	public override void UpdateGUI()
 	{
-		
+		GUI.Label(new Rect(5, 160, 60, 30), "Right");
+		_addRight = DrawIntSlider(_addRight, new Rect(5 + 65, 160, 60, 30));
+
+		GUI.Label(new Rect(5, 160 + 60, 60, 30), "Left");
+		_addLeft = DrawIntSlider(_addLeft, new Rect(5 + 65, 160 + 60, 60, 30));
+
+		GUI.Label(new Rect(5, 160 + 120, 60, 30), "Up");
+		_addUp = DrawIntSlider(_addUp, new Rect(5 + 65, 160 + 120, 60, 30));
+
+		GUI.Label(new Rect(5, 160 + 180, 60, 30), "Down");
+		_addDown = DrawIntSlider(_addDown, new Rect(5 + 65, 160 + 180, 60, 30));
+
+		if (GUI.Button(new Rect(5, 160 + 210, 330, 45), "APPLY"))
+		{
+
+		}
+	}
+
+	public static int DrawIntSlider(int number, Rect position)
+	{
+
+		string fieldText = GUI.TextField(new Rect(position.x + 30, position.y, 85, 25), number.ToString());
+
+		int answer;
+
+		//if we completely removed everything from the field, change it to 0
+		if (string.IsNullOrEmpty(fieldText))
+			fieldText = "0";
+
+		if(!int.TryParse(fieldText, out answer))
+			answer = number;
+
+		if (GUI.Button(new Rect(position.x, position.y, 25, 25), "-"))
+			answer -= 1;
+
+		if (GUI.Button(new Rect(position.x + 120, position.y, 25, 25), "+"))
+			answer += 1;
+
+		return answer;
 	}
 }
