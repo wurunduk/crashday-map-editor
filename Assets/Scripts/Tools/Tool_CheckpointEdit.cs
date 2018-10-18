26ee0c0089da185f9cb11e2b466a3b2bc79f0589
wurunDUK <wurunduk@gmail.com>
2018-10-18 23:36:43 +0300

    .

---
+++ Assets/Scripts/Tools/Tool_CheckpointEdit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool_CheckpointEdit : ToolGeneral
+{
+
+	public override void Initialize()
+	{
+		ToolName = "Edit Checkpoints";
+	}
+
+	public override void OnSelected() {}
+
+	public override void OnDeselected() {}
+
+	public override void OnRMBDown(Vector3 point) {}
+
+	public override void OnLMBDown(Vector3 point) {}
+
+	public override void OnLMB(Vector3 point) { }
+
+	public override void OnRMB(Vector3 point) { }
+
+	public override void OnMouseOverTile(IntVector2 point) {}
+
+	public override void Update() {}
+
+	public override void UpdateGUI(Rect guiRect) {}
+}
